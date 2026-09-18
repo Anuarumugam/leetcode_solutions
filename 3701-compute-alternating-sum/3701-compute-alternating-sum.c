5bce62b3d01018edcbb49aee04b211c8a206c606
ANU A <anuias0304@gmail.com>
2026-09-18 12:15:09 +0530

    Time: 0 ms (100%), Space: 10.1 MB (94.26%) - LeetHub

---
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.c
@@ -0,0 +1,17 @@
+int alternatingSum(int* nums, int numsSize) {
+    int sum=0;
+    for(int i=0;i<numsSize;i++)
+    {
+        if((i%2)==0)
+        {
+            sum=sum+nums[i];
+        }
+        else
+        {
+            sum=sum-nums[i];
+        
+        }
+
+    }
+    return sum;
+}
