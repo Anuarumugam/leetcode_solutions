d859fb6db43ff5b5fd11a880a660e3d52633303a
ANU A <anuias0304@gmail.com>
2026-09-21 22:44:36 +0530

    Time: 0 ms (100%), Space: 12.1 MB (12.54%) - LeetHub

---
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.c
@@ -0,0 +1,25 @@
+int numSubarrayProductLessThanK(int* nums, int numsSize, int k) {
+    if(k <= 1)
+    {
+        return 0;
+    }
+
+    int left = 0;
+    int product = 1;
+    int count = 0;
+
+    for(int right = 0; right < numsSize; right++)
+    {
+        product = product * nums[right];
+
+        while(product >= k)
+        {
+            product = product / nums[left];
+            left++;
+        }
+
+        count = count + (right - left + 1);
+    }
+
+    return count;
+}
