31e53a527af8fbaae3b5f1c424b40ecc70ffd469
ANU A <anuias0304@gmail.com>
2026-09-22 12:17:39 +0530

    Time: 0 ms (100%), Space: 8.8 MB (68.06%) - LeetHub

---
+++ 3794-reverse-string-prefix/3794-reverse-string-prefix.c
@@ -0,0 +1,18 @@
+
+    char* reversePrefix(char* s, int k)
+{
+    int i = 0;
+    int j = k - 1;
+
+    while(i < j)
+    {
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+
+        i++;
+        j--;
+    }
+
+    return s;
+}
