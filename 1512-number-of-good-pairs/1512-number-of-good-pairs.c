37209e33ce4f4bf15f97143f9a746569050aa618
ANU A <anuias0304@gmail.com>
2026-09-23 12:19:47 +0530

    Time: 0 ms (100%), Space: 8.9 MB (32.42%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.c
@@ -0,0 +1,13 @@
+int numIdenticalPairs(int* nums, int numsSize)
+{
+    int count[101] = {0};
+    int ans = 0;
+
+    for(int i = 0; i < numsSize; i++)
+    {
+        ans = ans + count[nums[i]];
+        count[nums[i]]++;
+    }
+
+    return ans;
+}
